1b79496c01c790e168634ed67c06e43d93830ec2
Ahamad-PC <ahamad.jedu@gmail.com>
2020-01-14 23:05:57 +0700

    validate

--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 
 router.get('/', function (req, res, next) {
@@ -7,15 +8,26 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/create', function (req, res, next) {
-    console.log(res);
-
-
-
+    //res.send(req.url);
+    //console.log(res);
     res.render('blog/create', { title: 'Create Blog' });
 });
 
-router.post('/create', function (req, res, next) {
-    console.log(res);
-});
+router.post('/create', [
+    check('title', 'กรุณาป้อนชื่อ').not().isEmpty(),
+    check('detail', 'เนื้อหา').not().isEmpty(),
+    check('author', 'ผู้แต่ง').not().isEmpty(),
+],
+    function (req, res, next) {
+        //console.log(res);
+        const result = validationResult(req);
+        var errors = result.errors;
+        if (!result.isEmpty()) {
+            //console.log(errors);
+            //return 
+            res.render('blog/create', { title: 'Create Blog', errors: errors });
+        }
+        console.log(req.body);
+    });
 
 module.exports = router;
